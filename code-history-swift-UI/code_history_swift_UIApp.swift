b95fa1fa964aec871bfb8e81e1ddba8855bda85b
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2022-01-15 20:36:15 +0300

    Initial commit

---
+++ code-history-swift-UI/code_history_swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_history_swift_UIApp.swift
+//  code-history-swift-UI
+//
+//  Created by Любовь Волкова on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct code_history_swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
