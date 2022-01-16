93969e35b95ff0f38d9df6224f6f9bf4c7f5e1de
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2022-01-16 14:26:43 +0300

    Question model added. DRY code.

---
+++ code-history-swift-UI/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  code-history-swift-UI
+//
+//  Created by Любовь Волкова on 16.01.2022.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
