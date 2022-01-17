5379d7473ed64dced74062cb12412e740996b0a5
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2022-01-17 20:02:16 +0300

    MVVM. Game model added.

---
+++ code-history-swift-UI/Models/Question.swift
@@ -0,0 +1,32 @@
+//
+//  Question.swift
+//  code-history-swift-UI
+//
+//  Created by Любовь Волкова on 16.01.2022.
+//
+
+import Foundation
+
+struct Question: Hashable {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0)    ]
+}
